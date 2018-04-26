b3882d5f724d0e048dab0b436f1b49b2dd6942a9
Blazej Hadzik <hadzikblazej@gmail.com>
2018-04-26 20:25:11 +1000

    Set User, not Person to be assigned relation in Case model.

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,6 +1,6 @@
 class Case < ApplicationRecord
   belongs_to :contact
-  belongs_to :assigned_user, class_name: 'Person'
+  belongs_to :assigned_user, class_name: 'User'
 
   validates :name, presence: true
   validates :contact, presence: true
