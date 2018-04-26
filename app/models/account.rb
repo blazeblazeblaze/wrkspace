152e4d8e45f6eea8fff4c6021e51003863f2b231
Blazej Hadzik <hadzikblazej@gmail.com>
2018-04-26 20:24:19 +1000

    Allow accounts to have multiple users assigned.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,8 @@
 class Account < ApplicationRecord
+  has_many :users
   has_many :contacts
   has_many :organizations, through: :contacts, source: 'contactable', source_type: 'Organization'
   has_many :people, through: :contacts, source: 'contactable', source_type: 'Person'
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Account do
   context 'associations' do
+    it { is_expected.to have_many(:users) }
     it { is_expected.to have_many(:contacts) }
     it { is_expected.to have_many(:organizations) }
     it { is_expected.to have_many(:people) }
@@ -9,5 +10,6 @@ RSpec.describe Account do
 
   context 'validations' do
     it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
   end
 end
