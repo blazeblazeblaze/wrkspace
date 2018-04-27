8640d1d3bab425e1840535b6abfc5c1d1dd674cb
Blazej Hadzik <hadzikblazej@gmail.com>
2018-04-27 21:45:37 +1000

    Update seeds. Create admin for default account.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
 facebook = Account.create(name: 'Facebook')
+
+facebook.users.create!(admin: true, email: 'admin@facebook.com', password: 'password', password_confirmation: 'password')
 
 20.times do
   facebook.contacts.create!(contactable: Organization.new(name: Faker::Company.name))
