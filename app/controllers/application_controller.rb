0b16744cf5e8f07d172c33008155694869240ee4
Blaze Hadzik <hadzikblazej@gmail.com>
2018-04-27 11:13:56 +1000

    Add devise authenticate_user! and custom devise layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   include AccountHelper
+  layout :layout_by_resource
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
 
   private
 
+  def layout_by_resource
+    return "devise" if devise_controller?
+    "application"
   end
 end
